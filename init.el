e911f5d6ead3f66c7325586ecf93f134049dbe04
Tim Fischbach <mail@timfischbach.de>
2010-09-20 13:23:25 +0200

    emacs-rails-reloaded

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+(add-to-list 'load-path "~/.emacs.d/vendor/rails-reloaded")
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file nil t)
--- vendor.el
+++ vendor.el
@@ -2,9 +2,15 @@
 (load "auctex" nil t)
 (load "preview-latex.el" nil t)
 
+(require 'rails-autoload)
+
 (autoload 'ebib "vendor/ebib" "Ebib, a BibTeX database manager." t)
 (autoload 'espresso-mode "vendor/espresso" nil t)
 (autoload 'git-status "vendor/git" "Entry point into git-status mode." t)
 (autoload 'git-blame-mode "vendor/git-blame" "Minor mode for incremental blame for Git." t)
+(autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode" t)
 (autoload 'ruby-mode "vendor/ruby-mode" "Major mode for ruby files" t)
+(autoload 'run-ruby "vendor/inf-ruby" "Run an inferior Ruby process" t)
 (autoload 'yaml-mode "vendor/yaml-mode" nil t)
+
+
