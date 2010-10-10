b0d4b3843f3b4a369999b2a6c328bbc20c74da74
Tim Fischbach <info@timfischbach.de>
2010-10-10 21:38:53 +0200

    ruby electric mode

--- etc/ruby.el
+++ etc/ruby.el
@@ -2,7 +2,9 @@
 (add-hook 'ruby-mode-hook
 	  '(lambda ()
 	    (outline-minor-mode 1)
+	    (ruby-electric-mode 1)
 	    (set (make-local-variable 'outline-regexp) " *\\(class \\|module \\|def \\|describe \\|context \\|it \\)")
+	    (define-key ruby-mode-map (kbd "RET") 'newline-and-indent)
 	    (define-key ruby-mode-map (kbd "C-<return>")
 	      (lambda ()
 		(interactive)
--- vendor.el
+++ vendor.el
@@ -9,6 +9,7 @@
 (autoload 'git-status "vendor/git" "Entry point into git-status mode." t)
 (autoload 'git-blame-mode "vendor/git-blame" "Minor mode for incremental blame for Git." t)
 (autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode" t)
+(autoload 'ruby-electric-mode "vendor/ruby-electric" nil t)
 (autoload 'ruby-mode "vendor/ruby-mode" "Major mode for ruby files" t)
 (autoload 'run-ruby "vendor/inf-ruby" "Run an inferior Ruby process" t)
 (autoload 'yaml-mode "vendor/yaml-mode" nil t)
