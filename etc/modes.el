07b457f485a5d7eabbafd46bf3fffb190707d763
Tim Fischbach <info@timfischbach.de>
2010-10-24 19:50:37 +0200

    ruby mode for Gemfile and Rakefile, html mode for handlebar files

--- etc/modes.el
+++ etc/modes.el
@@ -1,6 +1,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . espresso-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("/Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("/Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs$" . html-mode))
 
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
