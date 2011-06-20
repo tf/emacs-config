db6bd9c485e246c579a1d695f3061627110f7236
Tim Fischbach <tfischbach@codevise.de>
2011-06-20 20:42:18 +0200

    css mode for scss files

--- etc/modes.el
+++ etc/modes.el
@@ -6,5 +6,6 @@
 (add-to-list 'auto-mode-alist '("/Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs$" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
 
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
