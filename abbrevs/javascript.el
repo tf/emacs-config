25e457a5d72b03ab356eafb664eef9507d1e76a0
Tim Fischbach <tfischbach@codevise.de>
2012-10-27 21:12:51 +0200

    further abbrevs for js

--- abbrevs/javascript.el
+++ abbrevs/javascript.el
@@ -1,6 +1,9 @@
 
 (define-abbrev fundamental-mode-abbrev-table "ext" "" 'javascript-skeleton-class-extend)
 (define-abbrev fundamental-mode-abbrev-table "imm" "" 'javascript-skeleton-immediate-function)
+(define-abbrev fundamental-mode-abbrev-table "desf" "" 'javascript-skeleton-jasmine-describe)
+(define-abbrev fundamental-mode-abbrev-table "itf" "" 'javascript-skeleton-jasmine-it)
+(define-abbrev fundamental-mode-abbrev-table "prp" "" 'javascript-skeleton-constr-prop)
 
 (define-skeleton javascript-skeleton-class-extend
   "Insert class extend"
@@ -16,3 +19,23 @@
   "(function() {" \n
   >  _  \n
   -2 "}());")
+
+(define-skeleton javascript-skeleton-jasmine-describe
+  "Insert jasmine descibe"
+  nil
+  "describe('" _ "', function() {" \n
+  > \n
+  -2 "});")
+
+
+(define-skeleton javascript-skeleton-jasmine-it
+  "Insert jasmine it"
+  nil
+  "it('" _ "', function() {" \n
+  > \n
+  -2 "});")
+
+(define-skeleton javascript-skeleton-constr-prop
+  "Constructor property assignment"
+  "name: "
+  "this._" str " = " str ";")
