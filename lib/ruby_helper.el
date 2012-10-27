cbe8c6970a750f573fcb300430ce2294621ed322
Tim Fischbach <tfischbach@codevise.de>
2012-10-27 21:24:20 +0200

    ruby helper

---
+++ lib/ruby_helper.el
@@ -0,0 +1,52 @@
+
+(defun add-ruby-constructor-assignment-named (name)
+  ""
+  (interactive)
+  (save-excursion
+    (search-forward " end")
+    (newline-above)
+    (insert "@")
+    (insert name)
+    (insert " = ")
+    (insert name)))
+
+(defun add-ruby-constructor-assignment ()
+  ""
+  (interactive)
+  (add-ruby-constructor-assignment-named (thing-at-point 'word)))
+
+(defun add-ruby-attr-reader-named (name)
+  ""
+  (let ((contains-attr (save-excursion
+                         (goto-char (point-min))
+                         (re-search-forward "attr_\\(reader\\|writer\\)" nil t))))
+    (save-excursion
+      (if contains-attr
+          (progn
+            (message "foo")
+            (re-search-backward "attr_\\(reader\\|writer\\)")
+            (end-of-line)
+            (insert ", :")
+            (insert name))
+        (message "bar")
+        (search-backward "class ")
+        (newline-below)
+        (insert "attr_reader :")
+        (insert name)
+        (newline)))))
+
+(defun add-ruby-attr-reader ()
+  ""
+  (interactive)
+  (add-ruby-attr-reader-named (thing-at-point 'word)))
+
+(defun ruby-make-constructor ()
+  ""
+  (interactive)
+  (let ((current-line (buffer-substring-no-properties (line-beginning-position)
+                                                      (line-beginning-position 2))))
+    (string-match "(\\([^)]*\\))" current-line)
+    (mapcar '(lambda (a)
+               (add-ruby-attr-reader-named a)
+               (add-ruby-constructor-assignment-named a))
+            (split-string (match-string 1 current-line) ", "))))
